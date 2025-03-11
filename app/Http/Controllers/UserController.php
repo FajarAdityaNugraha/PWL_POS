b9f10ff95011dc980cec80f25a7e07f6b11d3c86
FajarAdityaNugraha <fajaradityanugraha01@gmail.com>
2025-03-11 22:00:49 +0700

    Praktikum 2.5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,21 +9,38 @@ class UserController extends Controller
 {
     public function index()
     {
-        // $data = [
-        //     'level_id' => 2,
-        //     'username' => 'manager_tiga',
-        //     'nama' => 'Manager 3',
-        //     'password' => Hash::make('12345')
-        // ];
-        $user = UserModel::firstOrNew(
+        $user = UserModel::create(
             [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
+                'username' => 'manager11',
+                'nama' => 'Manager11',
                 'password' => Hash::make('12345'),
                 'level_id' => 2
             ],
         );
+
+        $user->username = 'manager12';
+
+        // $user->isDirty();
+        // $user->isDirty('username');
+        // $user->isDirty('nama');
+        // $user->isDirty(['nama', 'username']);
+
+        // $user->isClean();
+        // $user->isClean('username');
+        // $user->isClean('nama');
+        // $user->isClean(['nama', 'username']);
+        // dd(user);
+
         $user->save();
-        return view('user', ['data' => $user]);
+
+        $user->wasChanged();
+        $user->wasChanged('username');
+        $user->wasChanged(['username', 'level_id']);
+        $user->wasChanged('nama');
+        dd($user->wasChanged(['nama', 'username']));
+
+        $user->isDirty();
+        $user->isClean();
+        // dd($user->isDirty());
     }
 }
